7d48fa59544aa89d05fdd947e7bf31f934c6c153
Brian Maloney <brian-maloney@users.noreply.github.com>
2018-07-09 01:04:32 +0000

    Generalized version, reads all sensors

--- yoctometeo.py
+++ yoctometeo.py
@@ -11,24 +11,10 @@ if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
 
 s = YSensor.FirstSensor()
 while s is not None:
-    (Gauge(s.get_functionId(), 'Current reading', ['unit', 'hardwareId'])
+    (Gauge(s.get_functionId(), 'Current {} reading'.format(s.get_functionId()), ['unit', 'hardwareId'])
         .labels(s.get_unit(), s.get_hardwareId())
         .set_function(s.get_currentValue))
     s = s.nextSensor()
-
-#t = YTemperature.FirstTemperature()
-#while t is not None:
-#    (Gauge('temperature', 'Current temperature', ['unit', 'hardwareId'])
-#        .labels(t.get_unit(), t.get_hardwareId())
-#        .set_function(t.get_currentValue))
-#    t = t.nextTemperature()
-
-#h = YHumidity.FirstHumidity()
-#while h is not None:
-#    (Gauge('humidity', 'Current humidity', ['unit', 'hardwareId'])
-#        .labels(t.get_unit(), h.get_hardwareId())
-#        .set_function(t.get_currentValue))
-#    h = h.nextTemperature()
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
